ff555fbaf4e0a8524407b6c037a0d43f2c3e5f8b
heeki <heekipark@gmail.com>
2018-08-16 17:09:43 -0400

    added test script directory

--- analytics/src/client.py
+++ analytics/src/client.py
@@ -1,8 +1,11 @@
 import json
+from utils.util import Util
 from utils.utilsocket import UtilSocket
 
 
 def main():
+    log = Util.get_logger("client")
+
     url = "ws://ws.blockchain.info/inv"
     url_raw_block = "https://blockchain.info/rawblock/"
     url_raw_tx = "https://blockchain.info/rawtx/"
@@ -12,20 +15,20 @@ def main():
     ping = {
         "op": "ping_block"
     }
-    print "message_payload={}".format(json.dumps(ping))
+    log.info("message_payload={}".format(json.dumps(ping)))
 
     response = usocket.send(json.dumps(ping))
-    print "ping_response={}".format(response)
+    log.info("ping_response={}".format(response))
 
     block = json.loads(response)
-    print "tx_count={}".format(len(block["x"]["txIndexes"]))
-    print "nTx={}".format(block["x"]["nTx"])
-    print "totalBTCSent={}".format(block["x"]["totalBTCSent"]/100000000)
-    print "height={}".format(block["x"]["height"])
-    print "foundBy.link={}".format(block["x"]["foundBy"]["link"])
+    log.info("tx_count={}".format(len(block["x"]["txIndexes"])))
+    log.info("nTx={}".format(block["x"]["nTx"]))
+    log.info("totalBTCSent={}".format(block["x"]["totalBTCSent"]/100000000))
+    log.info("height={}".format(block["x"]["height"]))
+    log.info("foundBy.link={}".format(block["x"]["foundBy"]["link"]))
 
     block_tx_id = block["x"]["foundBy"]["link"].split("/")[-1]
-    print "block_tx_id={}".format(block_tx_id)
+    log.info("block_tx_id={}".format(block_tx_id))
 
 
 if __name__ == "__main__":
---
+++ test/analytics.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PYTHONPATH=/Users/heeki/Documents/Isengard/code/btc_analytics/src
+cd /Users/heeki/Documents/Isengard/code/btc_analytics
+python analytics/src/client.py
